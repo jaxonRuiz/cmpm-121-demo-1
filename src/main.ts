cb910f087725c93b8c7d8bddc5be9cbd119518f3
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-10-04 19:08:21 -0700

    name change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "boop a doop";
 document.title = gameName;
 
 const header = document.createElement("h1");
