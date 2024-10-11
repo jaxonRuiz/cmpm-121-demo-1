c0e9ea1b32f2e26320c911512fe7f3f215e1037e
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-10-11 00:30:04 -0700

    step 8: changed narrative to collecting rocks

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "boop a doop";
+const gameName = "Rock Collector Simulator";
 let lastTime = performance.now();
 let FPS: number;
 document.title = gameName;
@@ -10,26 +10,25 @@ document.title = gameName;
 // creating page elements
 const header = document.createElement("h1");
 const clicker = document.createElement("button");
-const upgradeA = document.createElement("button");
-const upgradeB = document.createElement("button");
-const upgradeC = document.createElement("button");
+const volunteer_button = document.createElement("button");
+const intern_button = document.createElement("button");
+const worker_button = document.createElement("button");
 const buttonContainer = document.createElement("div");
 const textContainer = document.createElement("div");
 const scoreText = document.createElement("p");
 const rateText = document.createElement("p");
 const FPSText = document.createElement("p");
 
-clicker.innerHTML = "⚾";
-upgradeA.innerHTML = "Buy autoclicker (10)";
+clicker.innerHTML = "Pick up rock";
 header.innerHTML = gameName;
 
 // assembling page
 app.append(header);
+app.append(clicker);
 app.append(buttonContainer);
-buttonContainer.append(clicker);
-buttonContainer.append(upgradeA);
-buttonContainer.append(upgradeB);
-buttonContainer.append(upgradeC);
+buttonContainer.append(volunteer_button);
+buttonContainer.append(intern_button);
+buttonContainer.append(worker_button);
 app.append(textContainer);
 textContainer.append(scoreText);
 textContainer.append(rateText);
@@ -50,17 +49,17 @@ function update() {
 
   function buttonConditions() {
     // maybe could be done better with event target or something??
-    if (score < upgradeA_cost) upgradeA.disabled = true;
-    else upgradeA.disabled = false;
-    if (score < upgradeB_cost) upgradeB.disabled = true;
-    else upgradeB.disabled = false;
-    if (score < upgradeC_cost) upgradeC.disabled = true;
-    else upgradeC.disabled = false;
+    if (score < volunteer_cost) volunteer_button.disabled = true;
+    else volunteer_button.disabled = false;
+    if (score < intern_cost) intern_button.disabled = true;
+    else intern_button.disabled = false;
+    if (score < worker_cost) worker_button.disabled = true;
+    else worker_button.disabled = false;
   }
 
   function updateText() {
-    scoreText.innerHTML = `Score: ${score.toFixed(3)}`;
-    rateText.innerHTML = `Rate: ${total_rate.toFixed(3)}`;
+    scoreText.innerHTML = `Rocks Collected: ${score.toFixed(3)}`;
+    rateText.innerHTML = `Collection Rate: ${total_rate.toFixed(3)}`;
     FPS = 1000 / deltaTime(); // 1000ms = 1s
     FPSText.innerHTML = `FPS: ${FPS.toFixed(0)}`;
   }
@@ -79,43 +78,43 @@ function deltaTime() {
 let score: number = 0;
 const universal_price_multiplier: number = 1.15;
 let total_rate: number = 0;
-let upgradeA_cost: number = 10;
-let upgradeB_cost: number = 100;
-let upgradeC_cost: number = 1000;
-const upgradeA_rate: number = 0.1;
-const upgradeB_rate: number = 2;
-const upgradeC_rate: number = 50;
-let upgradeA_level: number = 0;
-let upgradeB_level: number = 0;
-let upgradeC_level: number = 0;
+let volunteer_cost: number = 10;
+let intern_cost: number = 100;
+let worker_cost: number = 1000;
+const volunteer_rate: number = 0.1;
+const intern_rate: number = 2;
+const worker_rate: number = 50;
+let volunteer_quantity: number = 0;
+let intern_quantity: number = 0;
+let worker_quantity: number = 0;
 
-upgradeA.innerHTML = `Buy upgradeA (${upgradeA_cost})`;
-upgradeB.innerHTML = `Buy upgradeB (${upgradeB_cost})`;
-upgradeC.innerHTML = `Buy upgradeC (${upgradeC_cost})`;
+volunteer_button.innerHTML = `Get Volunteer (${volunteer_cost})`;
+intern_button.innerHTML = `Obtain Intern (${intern_cost})`;
+worker_button.innerHTML = `Hire Worker (${worker_cost})`;
 
 // i know this is really bad, but i need to be on step 9 to fix. I will fix later.
-upgradeA.addEventListener("click", () => {
-  score -= upgradeA_cost;
-  upgradeA.innerHTML = `Autoclicker lvl ${upgradeA_level + 1} (${upgradeA_cost.toFixed(0)})`;
-  upgradeA_cost *= universal_price_multiplier;
-  upgradeA_level++;
-  total_rate += upgradeA_rate;
+volunteer_button.addEventListener("click", () => {
+  score -= volunteer_cost;
+  volunteer_button.innerHTML = `Number of Volunteers: ${volunteer_quantity + 1} (${volunteer_cost.toFixed(0)})`;
+  volunteer_cost *= universal_price_multiplier;
+  volunteer_quantity++;
+  total_rate += volunteer_rate;
 });
 
-upgradeB.addEventListener("click", () => {
-  score -= upgradeB_cost;
-  upgradeB.innerHTML = `Autoclicker lvl ${upgradeB_level + 1} (${upgradeB_cost.toFixed(0)})`;
-  upgradeB_cost *= universal_price_multiplier;
-  upgradeB_level++;
-  total_rate += upgradeB_rate;
+intern_button.addEventListener("click", () => {
+  score -= intern_cost;
+  intern_button.innerHTML = `Number of Interns: ${intern_quantity + 1} (${intern_cost.toFixed(0)})`;
+  intern_cost *= universal_price_multiplier;
+  intern_quantity++;
+  total_rate += intern_rate;
 });
 
-upgradeC.addEventListener("click", () => {
-  score -= upgradeA_cost;
-  upgradeC.innerHTML = `Autoclicker lvl ${upgradeC_level + 1} (${upgradeC_cost.toFixed(0)})`;
-  upgradeC_cost *= universal_price_multiplier;
-  upgradeC_level++;
-  total_rate += upgradeC_rate;
+worker_button.addEventListener("click", () => {
+  score -= volunteer_cost;
+  worker_button.innerHTML = `Number of Workers: ${worker_quantity + 1} (${worker_cost.toFixed(0)})`;
+  worker_cost *= universal_price_multiplier;
+  worker_quantity++;
+  total_rate += worker_rate;
 });
 
 // clicker functionality
